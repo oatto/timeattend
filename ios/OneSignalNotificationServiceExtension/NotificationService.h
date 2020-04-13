a0be0d7ec3e00d47af4c72906301b0c54d6c97de
WATCHDOGS <godoakbrutal@gmail.com>
2020-04-13 23:08:14 +0700

    Onesignal extension

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Godoak Brutal on 13/4/2563 BE.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
